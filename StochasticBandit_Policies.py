4fff2d25c1419772f4a8f0e40135f43912da87ae
AzOuss96 <oussa.azizi@gmail.com>
2020-04-28 18:33:25 +0200

    OOP coding

--- StochasticBandit_Policies.py
+++ StochasticBandit_Policies.py
@@ -1,65 +1,120 @@
 from __future__ import division
 import numpy as np
-from StochasticBandit_Modules import *
+#from StochasticBandit_Modules import *
+from __future__ import division
+import numpy as np
+from scipy.stats import beta
+import statsmodels.stats.proportion as ssp
+from utils import DivKL, klIC
 
 
-def ThompsonSampling(environment, horizon, alpha0, beta0):
-    (alphas, betas, gainTS, armsPlayed) = TS_initialize(np.size(environment), alpha0, beta0)
-    # Interaction with the environment
-    for t in range(horizon):
-        armToPlay = TS_recommendArm(alphas, betas)
-        reward = PlayBernoulliArm(environment[armToPlay])
-        #reward = int ((np.random.uniform() < environment[armToPlay]) == True)
-        (alphas, betas, gainTS, armsPlayed) = TS_receiveReward(alphas, betas, reward, armToPlay, gainTS, armsPlayed)
-    return armsPlayed
+
+class ThompsonSampling:
+	def __init__(self, K, alpha0, beta0):
+		if (alpha0 <=0 ):
+			raise ValueError
+		if (beta0 <=0 ):
+			raise ValueError
+		self.alphas = np.ones(K) * alpha0
+		self.betas = np.ones(K) * beta0
+		self.rewards = np.array([])
+		self.armsPlayed = []
+		self.t = 1
+	
+	def choose_arm(self):
+		theta = np.random.beta(self.alphas, self.betas) # Sampling the Beta distribution of each arm
+		self.t += 1
+		return np.argmax(theta)
+	
+	def update(self, reward, armToPlay):
+		self.rewards = np.append(self.rewards, reward)
+		self.armsPlayed.append(armToPlay)
+		if reward == 1:
+		    self.alphas[armToPlay] += 1
+		else:
+		    self.betas[armToPlay] += 1
 
 
-def BayesUCB(environment, horizon, alpha0, beta0, c, HF):
-    # HF: Horizon free option.
-    (alphas, betas, gainTS, armsPlayed) = TS_initialize(np.size(environment), alpha0, beta0)
-    # Interaction with the environment
-    for t in range(horizon):
-        armToPlay = BayesUCB_recommendArm(alphas, betas, t+1, horizon, c, HF)
-        reward = PlayBernoulliArm(environment[armToPlay])
-        (alphas, betas, gainTS, armsPlayed) = TS_receiveReward(alphas, betas, reward, armToPlay, gainTS, armsPlayed)
-    return armsPlayed
+class UCB:
+	def __init__(self, K, c):
+		self.ExpectedMeans = np.zeros(K)
+		self.NbrPlayArms = np.ones(K)
+		self.rewards = np.array([])
+		self.armsPlayed = []
+		self.c = c
+		self.t = 1
+
+	def update(self, reward, armToPlay):
+		self.rewards = np.append(self.rewards, reward)
+		self.armsPlayed.append(armToPlay)
+		self.ExpectedMeans[armToPlay] *=  self.NbrPlayArms[armToPlay]
+		self.NbrPlayArms[armToPlay] += 1
+		self.ExpectedMeans[armToPlay] += reward
+		self.ExpectedMeans[armToPlay] /= self.NbrPlayArms[armToPlay]
+
+	def choose_arm(self):
+		ucb = self.ExpectedMeans + np.sqrt(self.c * np.log(self.t) / self.NbrPlayArms)
+		self.t += 1
+		return np.argmax(ucb)
 
 
-def KLUCB(environment, horizon, c, HF):
-    (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_initialize(np.size(environment))
-    # Interaction with the environment
-    for t in range(horizon):
-        armToPlay = KLUCB_recommendArm(ExpectedMeans, NbrPlayArms, t+1, horizon, c, HF)
-        reward = PlayBernoulliArm(environment[armToPlay])
-        (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_receiveReward(ExpectedMeans, NbrPlayArms, reward, armToPlay, gainUCB, armsPlayed)
-    return armsPlayed
+class BayesUCB(ThompsonSampling):
+	def __init__(self, K, alpha0, beta0, HF, c, horizon):
+		super().__init__(K, alpha0, beta0)
+		self.HF = HF
+		self.c = c
+		self.horizon = horizon
+	
+	def choose_arm(self):
+		if self.HF:
+		    d = 1/(self.t * np.log(self.t+1)**self.c)
+		else:
+		    d = 1/(self.t * np.log(self.horizon)**self.c)
+		theta = beta.ppf(1 - d , self.alphas, self.betas)
+		self.t += 1
+		return np.argmax(theta)
 
 
-def CPUCB(environment, horizon, c):
-    (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_initialize(np.size(environment))
-    # Interaction with the environment
-    for t in range(horizon):
-        armToPlay = CPUCB_recommendArm(ExpectedMeans, NbrPlayArms, t+1, c)
-        reward = PlayBernoulliArm(environment[armToPlay])
-        (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_receiveReward(ExpectedMeans, NbrPlayArms, reward, armToPlay, gainUCB, armsPlayed)
-    return armsPlayed
+class KLUCB(UCB):
+	def __init__(self, K, horizon, c, HF):
+		super().__init__(K, c)
+		self.HF = HF
+		self.horizon = horizon
+
+	def SearchingKLUCBIndex(self):
+		p = self.ExpectedMeans
+		if self.HF:
+		    d = (np.log(self.t) + self.c * np.log(np.log(self.t + 1))) / self.NbrPlayArms
+		else:
+		    d = (np.log(t) + self.c * np.log(np.log(self.horizon))) / self.NbrPlayArms
+		return klIC(p,d)
+
+	def choose_arm(self):
+			indices = SearchingKLUCBIndex(self)
+			self.t += 1
+			return np.argmax(indices)
 
 
-def UCB(environment, horizon, c):
-    (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_initialize(np.size(environment))
-    # Interaction with the environment
-    for t in range(horizon):
-        armToPlay = UCB_recommendArm(ExpectedMeans, NbrPlayArms, t+1, c)
-        reward = PlayBernoulliArm(environment[armToPlay])
-        (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_receiveReward(ExpectedMeans, NbrPlayArms, reward, armToPlay, gainUCB, armsPlayed)
-    return armsPlayed
+class CPUCB(UCB):
+	def __init__(self, K, c):
+		super().__init__(K, c)
 
-def MOSS(environment, horizon):
-    (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_initialize(np.size(environment))
-    # Interaction with the environment
-    for t in range(horizon):
-        armToPlay = MOSS_recommendArm(ExpectedMeans, NbrPlayArms, horizon)
-        reward = PlayBernoulliArm(environment[armToPlay])
-        (ExpectedMeans, NbrPlayArms, gainUCB, armsPlayed) = UCB_receiveReward(ExpectedMeans, NbrPlayArms, reward, armToPlay, gainUCB, armsPlayed)
-    return armsPlayed
+	def choose_arm(self):
+		 _, ic = ssp.proportion_confint(np.floor(self.ExpectedMeans * self.NbrPlayArms), self.NbrPlayArms, 1/ (self.t * np.log(self.t+1)**self.c), method = 'beta')
+		self.t += 1
+		return np.argmax(ic)
+
+class MOSS(UCB):
+	def __init__(self K, c, horizon):
+		super().__init__(K, c)
+		self.horizon = horizon
+
+	def choose_arm(self):
+		K = np.size(self.NbrPlayArms)
+		ucb = self.ExpectedMeans + np.sqrt(np.maximum(np.log(self.horizon / (K * self.NbrPlayArms)),0) / self.NbrPlayArms)
+		self.t += 1
+		return np.argmax(ucb)
+
+
+
 
--- demoStochasticBandit.py
+++ demoStochasticBandit.py
@@ -1,7 +1,7 @@
 from __future__ import division
 import numpy as np
 from StochasticBandit_Policies import * 
-from StochasticBandit_Modules import *
+from utils import *
 import matplotlib.pyplot as plt
 
 
@@ -16,7 +16,7 @@ horizon = 1000 # Overall number of interactions with the environment
 
 """
 --------------------------------------------------------------------------------
-                                      Launching the stochastic bandits
+                                      Creating the stochastic bandits
 --------------------------------------------------------------------------------
 """
 
@@ -24,12 +24,28 @@ alpha0 = 1
 beta0 = 1
 c = 1
 HF = True # Horizon free option for BayesUCB and KLUCB
-armsPlayedTS = ThompsonSampling(environment, horizon, alpha0, beta0)
-armsPlayedBayesUCB = BayesUCB(environment, horizon, alpha0, beta0, c, HF)
-armsPlayedKLUCB = KLUCB(environment, horizon, c, HF)
-armsPlayedCPUCB = CPUCB(environment, horizon, c)
-armsPlayedMOSS = MOSS(environment, horizon)
-armsPlayedUCB = UCB(environment, horizon, c)
+K = np.size(environment)
+
+
+ts = ThompsonSampling(K, alpha0, beta0)
+ucb = UCB(K, c)
+bayesucb = BayesUCB(K, alpha0, beta0, HF, c, horizon)
+klucb = KLUCB(K, horizon, c, HF)
+cpucb = CPUCB(K, c)
+moss = MOSS(K, c, horizon)
+
+"""
+--------------------------------------------------------------------------------
+                                      Launching the stochastic bandits
+--------------------------------------------------------------------------------
+"""
+
+armsPlayedTS = play(ts, environment, horizon)
+armsPlayedBayesUCB = play(bayesucb, environment, horizon)
+armsPlayedKLUCB = play(klucb, environment, horizon)
+armsPlayedCPUCB = play(cpucb, environment, horizon)
+armsPlayedMOSS = play(moss, environment, horizon)
+armsPlayedUCB = play(ucb, environment, horizon)
 
 """
 ------------------------------------------------------------------------------
